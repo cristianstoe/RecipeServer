ced38ad30f76dd4cc9357eb0501c87979f29593b
vinirlmarques <viniciusrlmarques@gmail.com>
2023-12-06 15:15:59 -0300

    faltando get user by token

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 import fastify from 'fastify';
+import cors from '@fastify/cors'; // Atualizado para a nova versão
 import userRoutes from './routes/users.js';
 import recipeRoutes from './routes/recipes.js';
 
 const app = fastify();
+
+// Registre o plugin cors com as configurações padrão
+app.register(cors); // Sem mudanças aqui, mas agora está usando o novo pacote
 
 app.register(userRoutes);
 app.register(recipeRoutes);
 
 const PORT = process.env.PORT || 7600;
 
-app.listen(PORT, (err) => {
-  if (err) {
-    console.error(err);
-    process.exit(1);
-  }
-  console.log(`Server listening on http://localhost:${PORT}`);
+// Atualizado para a nova assinatura do método listen
+app.listen({ port: PORT, host: '0.0.0.0' }).then((address) => {
+  console.log(`Server listening on ${address}`);
+}).catch(err => {
+  console.error(err);
+  process.exit(1);
 });
